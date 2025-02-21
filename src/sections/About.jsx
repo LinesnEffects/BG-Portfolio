a5d5a2f99578c56fea0bf4b80e8945c298452c4c
LinesnEffects <bgf2311@outlook.com>
2025-02-21 17:42:14 -0500

    Finished about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,5 @@
 import Globe from "react-globe.gl";
+import Button from "../components/Button";
 
 const About = () => {
   return (
@@ -48,15 +49,51 @@ const About = () => {
                 showGraticules
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{
-                    lat: 7.07,
-                    lng: -73.07,
+                labelsData={[
+                  {
+                    lat: 7,
+                    lng: -73,
                     text: "I'm here!",
-                    color: 'white',
-                    size: 20,
-                }]}
+                    color: "white",
+                    size: 30,
+                  },
+                ]}
               />
             </div>
+            <div>
+              <p className="grid-headtext">
+                I work remotely across most timezones.
+              </p>
+              <p className="grid-subtext">
+                I'm based in Colombia, with remote work available.
+              </p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">My Drive for Coding</p>
+              <p className="grid-subtext">
+                I enjoy solving problems and building things through code. I'm
+                always looking for new challenges and opportunities to learn.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
           </div>
         </div>
       </div>
