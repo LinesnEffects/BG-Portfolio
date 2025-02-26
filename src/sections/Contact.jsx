bf34974f423a33dc40af96b47fefa2b7f09bdee6
LinesnEffects <bgf2311@outlook.com>
2025-02-26 13:38:40 -0500

    Finished experience section

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,118 @@
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+
+const Contact = () => {
+  const formRef = useRef();
+
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_vkfp64e",
+        "template_fvvatml",
+        {
+          from_name: form.name,
+          to_name: "Benjamin",
+          from_email: form.email,
+          to_email: "bgf2311@outlook.com",
+          message: form.message,
+        },
+        "3Znuluszs0rbck13R"
+      );
+
+      setLoading(false);
+
+      alert("Your message has been sent!");
+
+      setForm({ name: "", email: "", message: "" });
+    } catch (error) {
+      setLoading(false);
+
+      console.log(error);
+
+      alert("Something went wrong!");
+    }
+  };
+
+  return (
+    <section className="c-space my-20">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img
+          src="/assets/terminal.png"
+          alt="terminal background"
+          className="absolute inset-0 min-h-screen"
+        />
+        <div className="contact-container pt-16">
+          <h3 className="head-text">Let's talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            If you are looking to build a modern, responsive website, improve
+            your site, or need any help with your project, feel free to contact
+            me.
+          </p>
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="John Doe"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="johndoe@gmail.com"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Hi, I'm interested in..."
+              />
+            </label>
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? "Sending..." : "Send Message"}
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
