86065627370b63f761c686d4fb03c1a6c40375b6
LinesnEffects <bgf2311@outlook.com>
2025-03-03 11:59:20 -0500

    Finished footer and cleaned unsued files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./sections/navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
-import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 import Experience from "./sections/Experience";
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,18 +10,29 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
-            <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/>
-        </div>
-        <div className="social-icon">
-            <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2"/>
-        </div>
-        <div className="social-icon">
-            <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2"/>
-        </div>
+        <a href="https://github.com/LinesnEffects" target="_blank">
+          <div className="social-icon">
+            <img
+              src="/assets/github.svg"
+              alt="github"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/benjamin-gomez-039460284/" target="_blank">
+          <div className="social-icon">
+            <img
+              src="/assets/linkedin-logo.png"
+              alt="linkedin"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
       </div>
 
-      <p className="text-white-500">© 2024 Benjamin Gomez. All rights reserved.</p>
+      <p className="text-white-500">
+        © 2024 Benjamin Gomez. All rights reserved.
+      </p>
     </section>
   );
 };
